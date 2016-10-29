d2225de6271d3eb4bf638ee3ce7751a236c09528
ramon183 <ramonroc@gmail.com>
2016-10-29 14:39:51 -0200

    Add style and icons in navbars

--- resources/views/layouts/web/navbar.blade.php
+++ resources/views/layouts/web/navbar.blade.php
@@ -42,7 +42,7 @@
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                            aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            <i class="fa fa-user"></i> {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
