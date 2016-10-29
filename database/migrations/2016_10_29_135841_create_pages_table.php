b0e7b9263bfb6d87157d29aefcfadadbe40b9d9d
Ramon Schmidt Rocha <ramonroc@gmail.com>
2016-10-29 12:10:17 -0200

    Page Controller init, migration too

---
+++ database/migrations/2016_10_29_135841_create_pages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug');
+            $table->string('title');
+            $table->text('description');
+            $table->string('symbol', 25);
+            $table->string('color_symbol', 15);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pages');
+    }
+}
