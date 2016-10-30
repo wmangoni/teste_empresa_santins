d52d98d34c2cd7b752798f64b294d4d7a4dc7dc6
ramon183 <ramonroc@gmail.com>
2016-10-30 14:01:53 -0200

    Sections layout on website created

--- app/Http/Controllers/Web/PageController.php
+++ app/Http/Controllers/Web/PageController.php
@@ -2,21 +2,26 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Domains\Pages\SectionPageRepository;
 use Illuminate\Http\Request;
 use App\Domains\Pages\PageRepository;
 
 class PageController extends Controller
 {
     private $pageRepo;
+    private $sectionRepo;
 
-    public function __construct(PageRepository $pageRepo)
+    public function __construct(PageRepository $pageRepo, SectionPageRepository $sectionRepo)
     {
         $this->pageRepo = $pageRepo;
+        $this->sectionRepo = $sectionRepo;
     }
 
     public function show($slug = 'home')
     {
         $page = $this->pageRepo->getPage($slug);
-        return (count($page)) ? View('web.page_generated')->with('page', $page) : View('errors.204');
+        $sections = $this->sectionRepo->getSectionsFromPage($page->id);
+
+        return (count($page)) ? View('web.page_generated')->with('page', $page)->with('sections', $sections) : View('errors.204');
     }
 }
--- resources/views/web/page_generated.blade.php
+++ resources/views/web/page_generated.blade.php
@@ -2,10 +2,20 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-12 col-md-12  col-sm-12  col-xs-12">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="description">
                 {{$page->description}}
             </div>
         </div>
+    </div>
+    <div class="row">
+        @forelse($sections as $sec)
+            <div class="col-lg-{{$sec->col_lg}} col-md-{{$sec->col_md}} col-sm-{{$sec->col_sm}} col-xs-{{$sec->col_xs}}">
+                <div class="page-section" style="background-color: {{$sec->box_color}}" >
+                    {!! $sec->description !!}
+                </div>
+            </div>
+        @empty
+        @endforelse
     </div>
 @endsection
