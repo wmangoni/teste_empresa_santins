f94cd3509077575254595018ca9a9840ef9862f4
ramon183 <ramonroc@gmail.com>
2016-10-30 11:20:30 -0200

    Migration and seeder of sections page

---
+++ database/migrations/2016_10_30_131306_create_sections_page_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSectionsPageTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('sections_page', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->string('box_color')->nullable();
+            $table->integer('page_id')->unsigned();
+
+            $table->foreign('page_id')->references('id')->on('pages');
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('sections_page');
+    }
+}
