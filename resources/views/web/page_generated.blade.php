82ec8bf4c4033664d6452d8a0f183b4667999513
ramon183 <ramonroc@gmail.com>
2016-10-30 11:10:46 -0200

    Style changes in navbar and align context

--- resources/views/web/page_generated.blade.php
+++ resources/views/web/page_generated.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.web')
 
 @section('content')
-    <div class="col-lg-12 col-md-12  col-sm-12  col-xs-12">
-        <div class="description">
-            {{$page->description}}
+    <div class="row">
+        <div class="col-lg-12 col-md-12  col-sm-12  col-xs-12">
+            <div class="description">
+                {{$page->description}}
+            </div>
         </div>
     </div>
 @endsection
